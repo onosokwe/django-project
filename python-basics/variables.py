ef2c8c8c44b098070734451c718e39f5cf56711c
Andrew <onosokwe@gmail.com>
2024-08-15 13:55:34 +0100

    add: multiple assignment

--- python-basics/variables.py
+++ python-basics/variables.py
@@ -1,5 +1,21 @@
 # Comments
 
-'''
-Learning Variables in Python
-'''
+''' Learning Variables in Python '''
+
+"""
+
+Multiline comment for python
+
+"""
+
+# x = 8.9             # float
+# y = 9               # int
+# age = 90            # string
+isCorrect = True    # boolean
+is_false = False    # boolean
+
+z = str(x)          # casting float to string
+
+x, y, z = (8.9, 9, '8.9')
+
+print(x, y, type(z), isCorrect, is_false)
