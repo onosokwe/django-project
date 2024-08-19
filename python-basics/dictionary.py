7c834cee26e93a333b5abd9b18e0d4f3028dd3a3
Andrew <onosokwe@gmail.com>
2024-08-19 19:29:39 +0100

    dictionaries

--- python-basics/dictionary.py
+++ python-basics/dictionary.py
@@ -4,4 +4,30 @@
 
 # There is a huge difference between dictionary and lists
 # 
-#  
+# Create dictionary
+
+person = {
+    'first_name': 'Andrew',
+    'last_name': 'Okwe',
+    'age': 40
+}
+
+# print(person, type(person))
+
+# Create dictionary using constructor
+
+person2 = dict(first_name='Steph', lastName='Okwe')
+
+# print(person2, type(person2))
+
+# print(person['last_name'])
+# print(person.get("first_name"))
+
+# Add key/value to dictionary
+person['phone'] = '806-888-000-99'
+
+# print(person.get('phone'))
+
+# print(person.keys())
+
+print(person.items())
